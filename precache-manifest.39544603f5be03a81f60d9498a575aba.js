80fa3bf24a5390337b319bc2a1fb9c73d79a2321
Breno Scalzer <brenoscalzer1@gmail.com>
2020-08-21 14:56:14 -0300

    Updates

---
+++ precache-manifest.39544603f5be03a81f60d9498a575aba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f22170335e8e893fb92e482f359c1be",
+    "url": "/code-editor/index.html"
+  },
+  {
+    "revision": "3d5542b026ffaa532036",
+    "url": "/code-editor/static/css/main.f61b2acf.chunk.css"
+  },
+  {
+    "revision": "06c0204daa31376f75e3",
+    "url": "/code-editor/static/js/2.90f3649f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/code-editor/static/js/2.90f3649f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5542b026ffaa532036",
+    "url": "/code-editor/static/js/main.049d7ab7.chunk.js"
+  },
+  {
+    "revision": "aec8ca11e6cf34bee3ef",
+    "url": "/code-editor/static/js/runtime-main.a8467140.js"
+  }
+]);
